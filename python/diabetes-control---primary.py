ce1333c5780511e55c4cc263b36cacf919b03d08
Martin Chapman <contact@martinchapman.co.uk>
2024-05-24 18:01:04 +0100

    Update made by Phenoflow

---
+++ python/diabetes-control---primary.py
@@ -0,0 +1,25 @@
+# Evengelos Kontopantelis, David Reeves, Jose M Valderas, Stephen Campbell, Tim Doran, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"42W..00","system":"readv2"},{"code":"42WZ.00","system":"readv2"},{"code":"C10F711","system":"readv2"},{"code":"66AJz00","system":"readv2"},{"code":"42W3.00","system":"readv2"},{"code":"66AJ.00","system":"readv2"},{"code":"42c2.00","system":"readv2"},{"code":"42c0.00","system":"readv2"},{"code":"C108811","system":"readv2"},{"code":"C109711","system":"readv2"},{"code":"C109712","system":"readv2"},{"code":"42W1.00","system":"readv2"},{"code":"C10F700","system":"readv2"},{"code":"42c..00","system":"readv2"},{"code":"C108812","system":"readv2"},{"code":"C10E800","system":"readv2"},{"code":"66AI.00","system":"readv2"},{"code":"L2500PC","system":"readv2"},{"code":"L2500GC","system":"readv2"},{"code":"250 DC","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('diabetes-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["diabetes-control---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["diabetes-control---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["diabetes-control---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
